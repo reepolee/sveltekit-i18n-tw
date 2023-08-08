81be1ff57ea3c9f9466869078da77cac09e20f5e
Aleš Vaupotič <ales@vaupotic.eu>
2023-08-08 11:10:29 +0200

    removed console

--- src/routes/[[lang=lang]]/+layout.js
+++ src/routes/[[lang=lang]]/+layout.js
@@ -1,9 +1,6 @@
 import { locale } from 'svelte-i18n'
 export const load = async (event) => {
-
     let lang = event.params.lang ?? event.data.lang ?? null;
-
-    console.log('setting lang in layout to ', lang)
     locale.set(lang);
 
     return {
