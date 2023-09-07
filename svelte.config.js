471ffeabbcc0abfc85ef256a4d5df1ce9dd7e9ab
Aleš Vaupotič <ales@vaupotic.eu>
2023-09-07 15:44:23 +0200

    packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import adapter from "@sveltejs/adapter-node";
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
