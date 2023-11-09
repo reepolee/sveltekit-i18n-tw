582c53f7e71e9f192addcb090a9dbb032ce77735
Aleš Vaupotič <ales@vaupotic.eu>
2023-11-09 16:14:06 +0100

    safelist

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,6 @@
 const config = {
   darkMode: 'class',
   content: ["./src/**/*.{html,js,svelte,ts}"],
-  safelist: ["bg-white", "text-gray-900", "dark:bg-gray-900", "dark:text-gray-50"],
   theme: {
     extend: {},
   },
